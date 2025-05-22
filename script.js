53f386bcee757a94e267e35ce149aeb0beee30d0
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2025-05-22 21:50:53 +0600

    Added Light/Dark Mode

--- script.js
+++ script.js
@@ -36,6 +36,9 @@ const timerElement = document.getElementById('timer');
 const tryAgainButton = document.getElementById('try-again');
 const finalScoreElement = document.getElementById('final-score');
 
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const toggleIcon = document.getElementById('toggle-icon');
+
 let totalTyped = '';
 let currentCharIndex = 0;
 let errors = 0;
@@ -173,3 +176,38 @@ function showMobileMessage() {
 
 // Startup
 init();
+
+// Light-Dark Mode
+function toggleDarkLightMode(isDark) {
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    textContainer.style.backgroundColor = isDark ? 'rgb(255 255 255 / 25%)' : 'rgb(255 255 255)';
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') :
+       toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+}
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        toggleDarkLightMode(true);
+    }else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        toggleDarkLightMode(false);
+    }
+}
+
+// Switch Theme Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleDarkLightMode(true);
+    }
+}
